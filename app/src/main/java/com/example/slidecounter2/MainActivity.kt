6fb896f28e72599186e012810095700799ee0eb8
Ramires <Ramires1512@yandex.ru>
2021-03-19 18:37:43 +0300

    #

---
+++ app/src/main/java/com/example/slidecounter2/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.slidecounter2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.saif.slidecounter.SlideCounter
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val slideCounter: SlideCounter
+    }
+}
